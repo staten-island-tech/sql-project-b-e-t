daa751711b8ed391c5f105955d5a2e6580d3670e
timothyd93 <timothydeng93@gmail.com>
2023-05-12 13:59:30 -0400

    v

--- pupilpath-restoration/src/App.vue
+++ pupilpath-restoration/src/App.vue
@@ -15,7 +15,27 @@ onMounted(() => {
 </script>
 
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
+  <div id="container">
+    <div id="header"> 
+    <h1>Sign Up</h1>
+  <div class="info">
+    Username: <input type="text" v-model="username" />
+    <br>
+    Password: <input type="text" v-model="password" />
+    <button type="button">Submit</button>
+    <!-- <li v-for="country in countries" :key="country.id">{{ country.name }}</li> -->
+  </div>
+</div>
+  </div>
 </template>
+
+<style scoped>
+.info {
+  font-size: 20px;
+  margin: auto 5px;
+}
+#header {
+  font-size: 40px;
+}
+
+</style>
