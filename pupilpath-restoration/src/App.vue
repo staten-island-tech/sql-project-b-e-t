028694cd349e1a1dc819b2e0770d583580a8699e
timothyd93 <timothydeng93@gmail.com>
2023-05-16 14:00:30 -0400

    a

--- pupilpath-restoration/src/App.vue
+++ pupilpath-restoration/src/App.vue
@@ -1,42 +1,25 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
+import { onMounted, ref } from 'vue'
+import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
+import { supabase } from './supabase'
 
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
+const session = ref()
 
 onMounted(() => {
-  getCountries()
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
 })
 </script>
 
 <template>
-  <div id="container">
-    <div id="header"> 
-    <h1>Sign Up</h1>
-  <div class="info">
-    Username: <input type="text" v-model="username" />
-    <br>
-    Password: <input type="text" v-model="password" />
-    <br>
-    <button type="button">Submit</button>
-    <!-- <li v-for="country in countries" :key="country.id">{{ country.name }}</li> -->
-  </div>
-</div>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
   </div>
 </template>
-
-<style scoped>
-.info {
-  font-size: 20px;
-  margin: auto 5px;
-}
-#header {
-  font-size: 40px;
-}
-
-</style>
---
+++ pupilpath-restoration/src/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+export const supabase = createClient(
+  'https://lzkkvjvmjdgtdcpdpmzr.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6a2t2anZtamRndGRjcGRwbXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM4MjY2NjUsImV4cCI6MTk5OTQwMjY2NX0.BwwnXqFAhUye6OAa94uivgkpddJ0dp7S_TIszaJIo2o'
+)
