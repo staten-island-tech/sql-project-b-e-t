5be672bace7ac08f5667ca926af40be6baea4237
BrianZ26 <brian.zhao026@gmail.com>
2023-05-23 13:27:28 -0400

    Login

--- pupilpath-restoration/src/App.vue
+++ pupilpath-restoration/src/App.vue
@@ -4,7 +4,8 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div id="container" false>
-    <RouterLink to="/" draggable="false" class="router">Sign/Log in</RouterLink>
+    <RouterLink to="/" draggable="false" class="router">Sign Up</RouterLink>
+    <RouterLink to="/Login" draggable="false" class="router">Log In</RouterLink>
     <RouterLink to="/Grades" draggable="false" class="router">Grades</RouterLink>
     <RouterView />
   </div>
