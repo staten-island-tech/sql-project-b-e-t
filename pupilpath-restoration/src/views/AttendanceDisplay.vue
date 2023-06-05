d54747e43d3d59157c3584d3e551aea1c418d54c
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2023-06-05 19:01:52 -0400

    implemented calendar

--- pupilpath-restoration/src/views/AttendanceDisplay.vue
+++ pupilpath-restoration/src/views/AttendanceDisplay.vue
@@ -1,55 +1,144 @@
 <template>
-  <VCalendar :attributes="attributes" />
+  <VDatePicker v-model="date" mode="date" is-required :attributes="attributes" />
+  <div>
+    <label class="text" for="dateSelected">Date Selected</label>
+    <br>
+    <label id="dateSelected">{{ cleanDate }}</label>
+    <br>
+    <br>
+    <label class="text" for="attendance">Mark the day as:</label>
+    <br>
+    <button class="green" id="attendance" value="present"
+      @click="markAttendance('green', date, 'Present')">Present</button>
+    <button class="yellow" id="attendance" value="tardy" @click="markAttendance('yellow', date, 'Tardy')">Tardy</button>
+    <button class="red" id="attendance" value="absent" @click="markAttendance('red', date, 'Absent')">Absent</button>
+    <button @click="clearAttendance(date)">Clear Attendance</button>
+
+  </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { supabase } from '../supabase'
+import { onMounted, ref, toRefs } from 'vue'
 
-const todos = ref([
-  {
-    description: 'Take Noah to basketball practice.',
-    isComplete: false,
-    dates: { weekdays: 6 }, // Every Friday
-    color: 'red',
-  },
-]);
 
-const attributes = computed(() => [
-  // Attributes for todos
-  ...todos.value.map(todo => ({
-    dates: todo.dates,
-    dot: {
-      color: todo.color,
-      class: todo.isComplete ? 'opacity-75' : '',
-    },
-    popover: {
-      label: todo.description,
-    },
-  })),
-]);
-</script>
+onMounted(() => {
+  getAttributes()
+})
 
-  <style>
-.grid{
-  display: grid;
-  grid-template-columns: auto auto auto auto auto auto auto;
-  background-color: #2196F3;
-  padding: 10px;
+const props = defineProps(['session'])
+const { session } = toRefs(props)
+
+const date = ref(new Date());
+
+let cleanDate = date.value.toString().split(" ").slice(0, 4).join(" ");;
+//Listens for date clicks
+addEventListener("click", () => { });
+onclick = () => {
+  //Get clean date from clicked date
+  cleanDate = date.value.toString().split(" ").slice(0, 4).join(" ");
+  console.log(cleanDate);
+  document.getElementById("dateSelected").innerHTML = cleanDate;
+};
+
+
+let attributes = ref([])
+
+async function markAttendance(value, date, attendance) {
+  try {
+    //check if selected date is not already in list of attributes
+    for (const attrs in attributes.value) {
+      if (attributes.value[attrs].dates == date.toDateString()) {
+        //change the color of the bar
+        attributes.value[attrs].bar = value;
+        updateAttributes();
+        return;
+      }
+    }
+    const newAttr = {
+      bar: value,
+      dates: date.toDateString(),
+      popover: {
+        label: attendance,
+        visibility: "hover",
+        placement: "top",
+        showPointer: true,
+        showArrow: true,
+      },
+    }
+    attributes.value.push(newAttr);
+    updateAttributes();
+    //console.log(date.toDateString());
+    //console.log(attributes.value);
+  }
+  catch (error) {
+    alert(error.message);
+  }
 }
-.grid-item {
-  background-color: rgba(255, 255, 255, 0.8);
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  padding: 20px;
-  font-size: 30px;
-  text-align: center;
-}
 
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
+function clearAttendance(date) {
+  for (const attrs in attributes.value) {
+    if (attributes.value[attrs].dates == date.toDateString()) {
+      delete attributes.value[attrs];
+      updateAttributes();
     }
   }
-  </style>
-  
+
+}
+
+async function getAttributes() {
+  try {
+    const { user } = session.value
+
+    let { data, error, status } = await supabase
+      .from('attendance')
+      .select(`attributes`)
+      .eq('id', user.id)
+
+    if (error && status !== 406) throw error
+
+    if (data) {
+      console.log(data.attributes);
+      attributes.value = data.attributes
+    }
+  } catch (error) {
+    alert(error.message)
+  }
+}
+
+async function updateAttributes(){
+  try {
+    const { user } = session.value
+
+    const updates = {
+      id: user.id,
+      attributes: attributes.value,
+    }
+
+    let { error } = await supabase.from('attendance').upsert(updates)
+
+    if (error) throw error
+  } catch (error) {
+    alert(error.message)
+  }
+}
+
+</script>
+
+<style>
+.text {
+  font-size: 20px;
+}
+
+.green {
+  background-color: green;
+}
+
+.yellow {
+  background-color: yellow;
+}
+
+.red {
+  background-color: red;
+}
+</style>
